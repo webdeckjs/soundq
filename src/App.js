a789c8f85677e141612599833faad5734415acb2
Cezarijus Kivylius <ltcezaris@gmail.com>
2024-01-27 22:27:27 +0000

    add simple onclick

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const App = ({ title }) => (
-  <div
-    style={{
-      borderRadius: '4px',
-      padding: '2em',
-      backgroundColor: 'red',
-      color: 'white',
-    }}
-    data-e2e="APP_2__WIDGET"
-  >
-    {title}
-  </div>
-);
+const App = ({ title, onClick }) => {
+
+  useEffect(() => {
+    if(onClick){
+      alert('hello');
+    }
+  }, [onClick])
+
+  return (
+    <div
+      style={{
+        borderRadius: '4px',
+        padding: '2em',
+        color: 'white',
+      }}
+      data-e2e="APP_2__WIDGET"
+    >
+      this will alert user when button is clicked
+    </div>
+  )
+};
 
 export default App;
