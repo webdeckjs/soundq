ca351fcd89c9cb539b0f0037782d37ae370b092f
OzzyD1 <ozzydomarkas@gmail.com>
2024-06-26 22:26:40 +0100

    added filter

--- src/App.js
+++ src/App.js
@@ -3,19 +3,29 @@ import React, { useEffect } from "react";
 const context = new AudioContext();
 
 export const onPress = ({ config }) => {
-    const o = context.createOscillator();
-    const g = context.createGain();
-    g.gain.setValueAtTime(config.gain, context.currentTime);
-    o.type = config.waveform || "sine";
-    o.frequency.value = config.frequency || 100;
-    o.connect(g);
-    g.connect(context.destination);
-    o.start(0);
-    g.gain.exponentialRampToValueAtTime(
-        config.ramp || 1.0,
-        context.currentTime + 2
-    );
-    o.stop(context.currentTime + 2);
+    // Create Nodes
+    const osc = context.createOscillator();
+    const gain = context.createGain();
+    const filter = context.createBiquadFilter();
+
+    filter.frequency.setValueAtTime(config.filterFreq, context.currentTime, 0);
+    gain.gain.setValueAtTime(config.gain, context.currentTime);
+    osc.type = config.waveform || "sine";
+    osc.frequency.value = config.frequency || 100;
+
+    // Connect Nodes
+    osc.connect(filter);
+    filter.connect(context.destination);
+    osc.connect(gain);
+    gain.connect(context.destination);
+
+    // Play
+    osc.start(0);
+    // g.gain.exponentialRampToValueAtTime(
+    //     config.ramp || 1.0,
+    //     context.currentTime + 2
+    // );
+    osc.stop(context.currentTime + 2);
 };
 
 const App = ({ config, setConfig }) => {
@@ -30,7 +40,7 @@ const App = ({ config, setConfig }) => {
                 <h3>SoundQ Oscillator Plugin</h3>
                 <div className="settings">
                     <div className="setting">
-                        <label htmlFor="waveform">waveform: </label>
+                        <label htmlFor="waveform">Waveform: </label>
                         <select
                             required
                             name="waveform"
@@ -46,8 +56,27 @@ const App = ({ config, setConfig }) => {
                             <option value="triangle">triangle</option>
                         </select>
                     </div>
+                    {/* Filter Type Select */}
                     <div className="setting">
-                        <label htmlFor="frequency">frequency: </label>
+                        <label htmlFor="filterType">Filter Type: </label>
+                        <select
+                            required
+                            name="filterType"
+                            onChange={onChange}
+                            value={config.filterType || ""}
+                        >
+                            <option value="" disabled hidden>
+                                lowpass
+                            </option>
+                            <option value="lowpass">Low Pass</option>
+                            <option value="highpass">High Pass</option>
+                            <option value="bandpass">Band Pass</option>
+                            <option value="lowshelf">Low Shelf</option>
+                        </select>
+                    </div>
+                    {/* Oscilliator Freq */}
+                    <div className="setting">
+                        <label htmlFor="frequency">Frequency: </label>
                         <input
                             type="number"
                             name="frequency"
@@ -55,6 +84,21 @@ const App = ({ config, setConfig }) => {
                             onChange={onChange}
                             value={config.frequency || ""}
                         />
+                    </div>
+                    {/* Filter Freq Select */}
+                    <div className="setting">
+                        <label htmlFor="filterFreq">Filter Freq: </label>
+                        <input
+                            type="range"
+                            name="filterFreq"
+                            min="30"
+                            max="1000"
+                            step="1"
+                            onChange={onChange}
+                            value={config.filterFreq || 1.0}
+                        />
+                        {parseFloat(config.filterFreq || 1.0).toFixed(0)}
+                        {/* <input type="number" name="ramp" placeholder="1.0" onChange={onChange} value={config.ramp || ""} /> */}
                     </div>
                     <div className="setting">
                         <label htmlFor="ramp">Ramp: </label>
