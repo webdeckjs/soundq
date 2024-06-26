8cb18f92efa65b613e44a8effa3c4295c580332f
OzzyD1 <ozzydomarkas@gmail.com>
2024-06-26 21:37:36 +0100

    added gain and changed title

--- src/App.js
+++ src/App.js
@@ -1,55 +1,93 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 const context = new AudioContext();
 
 export const onPress = ({ config }) => {
-  const o = context.createOscillator();
-  const g = context.createGain();
-  o.type = config.waveform || "sine";
-  o.frequency.value = config.frequency || 100;
-  o.connect(g);
-  g.connect(context.destination);
-  o.start(0)
-  g.gain.exponentialRampToValueAtTime(config.ramp || 1.0, context.currentTime + 2)
-  o.stop(context.currentTime + 2);
-}
+    const o = context.createOscillator();
+    const g = context.createGain();
+    g.gain.setValueAtTime(config.gain, context.currentTime);
+    o.type = config.waveform || "sine";
+    o.frequency.value = config.frequency || 100;
+    o.connect(g);
+    g.connect(context.destination);
+    o.start(0);
+    g.gain.exponentialRampToValueAtTime(
+        config.ramp || 1.0,
+        context.currentTime + 2
+    );
+    o.stop(context.currentTime + 2);
+};
 
 const App = ({ config, setConfig }) => {
+    const onChange = (event) => {
+        console.log(event.target.name, event.target.value);
+        setConfig({ ...config, [event.target.name]: event.target.value });
+    };
 
-  const onChange = (event) => {
-    console.log(event.target.name, event.target.value);
-    setConfig({ ...config, [event.target.name]: event.target.value });
-  }
-
-  return (
-    <div style={{ height: "100%", background: "pink"}} >
-      <div style={{ padding: "2em" }}>
-        <h3>SoundQ Plugin</h3>
-        <div className="settings">
-          <div className="setting">
-            <label htmlFor="waveform">waveform: </label>
-            <select required name="waveform" onChange={onChange} value={config.waveform || ""}>
-              <option value="" disabled hidden>sine</option>
-              <option value="sine">sine</option>
-              <option value="square">square</option>
-              <option value="sawtooth">sawtooth</option>
-              <option value="triangle">triangle</option>
-            </select>
-          </div>
-          <div className="setting">
-            <label htmlFor="frequency">frequency: </label>
-            <input type="number" name="frequency" placeholder="403" onChange={onChange} value={config.frequency || ""} />
-          </div>
-          <div className="setting">
-            <label htmlFor="ramp">exponential ramp: </label>
-            <input type="range" name="ramp" min="0.0001" max="1"  step="0.0001" onChange={onChange} value={config.ramp || 1.0} /> 
-            {parseFloat(config.ramp || 1.0).toFixed(4)}
-            {/* <input type="number" name="ramp" placeholder="1.0" onChange={onChange} value={config.ramp || ""} /> */}
-          </div>
+    return (
+        <div style={{ height: "100%", background: "pink" }}>
+            <div style={{ padding: "2em" }}>
+                <h3>SoundQ Oscillator Plugin</h3>
+                <div className="settings">
+                    <div className="setting">
+                        <label htmlFor="waveform">waveform: </label>
+                        <select
+                            required
+                            name="waveform"
+                            onChange={onChange}
+                            value={config.waveform || ""}
+                        >
+                            <option value="" disabled hidden>
+                                sine
+                            </option>
+                            <option value="sine">sine</option>
+                            <option value="square">square</option>
+                            <option value="sawtooth">sawtooth</option>
+                            <option value="triangle">triangle</option>
+                        </select>
+                    </div>
+                    <div className="setting">
+                        <label htmlFor="frequency">frequency: </label>
+                        <input
+                            type="number"
+                            name="frequency"
+                            placeholder="403"
+                            onChange={onChange}
+                            value={config.frequency || ""}
+                        />
+                    </div>
+                    <div className="setting">
+                        <label htmlFor="ramp">Ramp: </label>
+                        <input
+                            type="range"
+                            name="ramp"
+                            min="0.0001"
+                            max="1"
+                            step="0.0001"
+                            onChange={onChange}
+                            value={config.ramp || 1.0}
+                        />
+                        {parseFloat(config.ramp || 1.0).toFixed(4)}
+                        {/* <input type="number" name="ramp" placeholder="1.0" onChange={onChange} value={config.ramp || ""} /> */}
+                    </div>
+                    <div className="setting">
+                        <label htmlFor="ramp">Gain: </label>
+                        <input
+                            type="range"
+                            name="gain"
+                            min="0.0001"
+                            max="1"
+                            step="0.0001"
+                            onChange={onChange}
+                            value={config.gain || 1.0}
+                        />
+                        {parseFloat(config.gain || 1.0).toFixed(4)}
+                        {/* <input type="number" name="ramp" placeholder="1.0" onChange={onChange} value={config.ramp || ""} /> */}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    );
 };
 
 export default App;
